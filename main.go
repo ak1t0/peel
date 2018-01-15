cfe25e704b7f86d8aa7eb436bb31f4e8c9ae7578
ak1t0 <aktoo3097@gmail.com>
2018-01-15 21:48:56 +0900

    add cli

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/urfave/cli"
+	"os"
 )
 
+// Version number
+var Version = "0.0.1"
+
 func main() {
-	fmt.Println("Hello, Onion")
+	app := cli.NewApp()
+	app.Name = "peel"
+	app.Usage = "scan offensively onion service"
+	app.Version = Version
+	app.Author = "ak1t0"
+	app.Email = "aktoo3097@gmail.com"
+	//app.Commands = Commands
+	app.Copyright = "(c) 2018 ak1t0"
+
+	app.Run(os.Args)
 }
